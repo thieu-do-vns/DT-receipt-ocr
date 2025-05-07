6a15846add5a0aebec6e52ee3dac846b2ded3f14
huuthieu <huuthieu@huuthieus-MacBook-Pro.local>
2025-05-07 15:49:27 +0700

    fix(ocr): Update prompt to get receipt no

--- llm_extract.py
+++ llm_extract.py
@@ -16,13 +16,13 @@ json_schema = Receipt.model_json_schema()
 def process_document_with_ai(document_text):
     # Initialize the client pointing to your local server
     client = OpenAI(
-        base_url="",
-        api_key=""  # Your local server may not require an API key
+        base_url="http://43.218.227.242:8000/v1",
+        api_key="hello-from-vns"  #
     )
     
     # Make the API call
     response = client.chat.completions.create(
-        model="microsoft/Phi-4-mini-instruct",
+        model="RedHatAI/Mistral-Small-3.1-24B-Instruct-2503-quantized.w4a16",
         messages=[
         {"role": "system", "content": """You are a helpful assistant. Extract information EXACTLY as it appears in the provided text, without combining with other texts. 
         Return only a single valid JSON object with the shipping details, without any additional text, comments, or trailing content"""},
@@ -37,11 +37,13 @@ def process_document_with_ai(document_text):
 
         CONTEXT:
         1. Process ONLY the text provided in this single request
-        2. For destination country, preserve the EXACT text format - do not separate or modify country names
+        2. Receipt number should have format NP****
+        3. For destination country, preserve the EXACT text format - do not separate or modify country names
         For example, if text contains 'Youyiguan CHINAQuang Binh VIETNAM', return 'Youyiguan CHINA, Quang Binh VIETNAM'
         Do not consider phrases like 'IMPORT AND EXPORT TRADE' as destination countries
-        3. Number of boxes maybe have unit of CARTONS (cartons)
-        4. Export date should be near to phrase: Date of exportation and have format dd/mm/yyyy
+        4. Transportation may be start with by, for example: By Train
+        5. Number of boxes maybe have unit of CARTONS (cartons)
+        6. Export date should be near to phrase: Date of exportation and have format dd/mm/yyyy
 
         Return only a single valid JSON object with the shipping details, without any additional text, comments, or trailing content
 
